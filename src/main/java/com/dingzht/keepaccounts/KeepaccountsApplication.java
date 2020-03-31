a53c16cc32c998dcc97e926f08db97836f9d6eb1
dingzht <dingzht@wellcom.cn>
2020-03-31 16:27:16 +0800

    spring boot init

---
+++ src/main/java/com/dingzht/keepaccounts/KeepaccountsApplication.java
@@ -0,0 +1,13 @@
+package com.dingzht.keepaccounts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeepaccountsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KeepaccountsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dingzht/keepaccounts/KeepaccountsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dingzht.keepaccounts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeepaccountsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
